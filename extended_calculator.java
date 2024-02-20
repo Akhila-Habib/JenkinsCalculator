787c4d7ba6ee44bdf17f7845ac500d1e830b282c
01fe22bca167@kletech.ac.in <01fe22bca167@kletech.ac.in>
2024-02-20 10:35:42 +0530

    code for extended calculator Ops

---
+++ extended_calculator.java
@@ -0,0 +1,9 @@
+class ExtendedCalculator{
+    float square(float operand_1){
+        return(operand_1 * operand_1);
+    }
+
+    float cube (float operand_1){
+        return(operand_1 * operand_1 * operand_1);
+    }
+}
